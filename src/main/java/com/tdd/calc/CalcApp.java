ccfcb7cf6912de962887287514d2a1a379a2348e
ohgil <ohgilhwan1957@gmail.com>
2023-08-10 10:32:00 +0900

    minus

--- src/main/java/com/tdd/calc/CalcApp.java
+++ src/main/java/com/tdd/calc/CalcApp.java
@@ -2,9 +2,26 @@ package com.tdd.calc;
 
 public class CalcApp {
     public static int run(String exp) {
-        String[] bits = exp.split(" \\+ ");
+        boolean needToPlus = exp.contains("+");
+        boolean needToMinus = exp.contains("-");
+
+        String[] bits = null;
+
+        if (needToPlus) {
+            bits = exp.split(" \\+ ");
+        } else if (needToMinus) {
+            bits = exp.split(" \\- ");
+        }
+
         int a = Integer.parseInt(bits[0]);
-        int b = Integer.parseInt(bits[0]);
-        return a + b;
+        int b = Integer.parseInt(bits[1]);
+
+        if (needToPlus) {
+            return a + b;
+        } else if (needToMinus) {
+            return a - b;
+        }
+
+        throw new RuntimeException("올바른 계산식이 아닙니다.");
     }
 }
